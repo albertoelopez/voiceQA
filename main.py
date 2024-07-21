3e8c7c532c306f6c61dc5128e140bda7c26383ff
SantiR <santiriosolaya@gmail.com>
2024-07-21 02:29:52 -0500

    fix: main.py execution
    Signed-off-by: SantiR <santiriosolaya@gmail.com>

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from textwrap import dedent
 from crewai import Crew
 from dotenv import load_dotenv
 
-from voice_analysis_agents import VoiceAnalysisAgents
+from voice_analysis_agents import ConversationAnalysisAgents
 from voice_analysis_tasks import VoiceAnalysisTasks
 
 load_dotenv()
 
 class VoiceAnalysisCrew:
-  def __init__(self, company):
-    self.company = company
+  # def __init__(self, company):
+  #   self.company = company
 
   def run(self):
-    agents = VoiceAnalysisAgents()
+    agents = ConversationAnalysisAgents()
     tasks = VoiceAnalysisTasks()
 
     conversation_analyst_agent = agents.conversation_evaluator()
     human_agent = agents.human_agent()
     chatbot_agent = agents.chatbot_agent()
     
-    chatbot_task = tasks.chatbot_analysis_task(conversation_analyst_agent, self.company, "")
+    chatbot_task = tasks.chatbot_analysis_task(chatbot_agent)
     human_task = tasks.human_analysis_task(human_agent)
-    conversation_task = tasks.conversation_context_task(chatbot_agent)
+    conversation_task = tasks.conversation_context_task(conversation_analyst_agent, human_task, chatbot_task)
 
     crew = Crew(
       agents=[
@@ -45,13 +45,13 @@ if __name__ == "__main__":
   print("## Welcome to Financial Analysis Crew")
   print('-------------------------------')
   # we need to define the inputs here
-  company = input(
-    dedent("""
-      What is the company you want to analyze?
-    """))
+  # company = input(
+  #   dedent("""
+  #     What is the company you want to analyze?
+  #   """))
   
-  financial_crew = VoiceAnalysisCrew(company)
-  result = financial_crew.run()
+  voice_qa_crew = VoiceAnalysisCrew()
+  result = voice_qa_crew.run()
   print("\n\n########################")
   print("## Here is the Report")
   print("########################\n")
